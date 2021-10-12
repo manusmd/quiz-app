01df599fcff876a30b2bcc02dd634308e2b03d46
“manusmd” <manuel.schmid@js-tut.de>
2021-10-12 12:19:03 +0200

    Add functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,30 @@
-const correctAnswer = true;
+const correctAnswer = false;
 
 const randomQuestion = document.querySelector(".question");
 randomQuestion.textContent = "Are you a robot?";
 
 document.querySelector(".true").onclick = function () {
   if (correctAnswer === true) {
+    showAnswerIsCorrect();
+  } else {
+    showAnswerIsIncorrect();
+  }
+
+  document.querySelector(".false").onclick = function () {
+    if (correctAnswer === false) {
+      showAnswerIsCorrect();
+    } else {
+      showAnswerIsIncorrect();
+    }
+  };
+
+  function showAnswerIsCorrect() {
     const answer = document.createElement("p");
     answer.textContent = "That's correct";
     answer.className = "correct";
-    document.body.append(answer);
-  } else {
-    const answer = document.createElement("p");
-    answer.textContent = "That's incorrect";
-    answer.className = "incorrect";
     document.body.append(answer);
   }
-};
-
-document.querySelector(".false").onclick = function () {
-  if (correctAnswer === false) {
-    const answer = document.createElement("p");
-    answer.textContent = "That's correct";
-    answer.className = "correct";
-    document.body.append(answer);
-  } else {
+  function showAnswerIsIncorrect() {
     const answer = document.createElement("p");
     answer.textContent = "That's incorrect";
     answer.className = "incorrect";
