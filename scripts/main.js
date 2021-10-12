d14adb90d317c61aa278999ad5c7afbd72d1c625
“manusmd” <manuel.schmid@js-tut.de>
2021-10-12 14:44:29 +0200

    Disable buttons on answer

--- scripts/main.js
+++ scripts/main.js
@@ -3,31 +3,40 @@ const correctAnswer = false;
 const randomQuestion = document.querySelector(".question");
 randomQuestion.textContent = "Are you a robot?";
 
-document.querySelector(".true").onclick = function () {
+const yesButton = document.querySelector(".yes");
+yesButton.onclick = function () {
   if (correctAnswer === true) {
     showAnswerIsCorrect();
   } else {
     showAnswerIsIncorrect();
   }
+};
 
-  document.querySelector(".false").onclick = function () {
-    if (correctAnswer === false) {
-      showAnswerIsCorrect();
-    } else {
-      showAnswerIsIncorrect();
-    }
-  };
-
-  function showAnswerIsCorrect() {
-    const answer = document.createElement("p");
-    answer.textContent = "That's correct";
-    answer.className = "correct";
-    document.body.append(answer);
-  }
-  function showAnswerIsIncorrect() {
-    const answer = document.createElement("p");
-    answer.textContent = "That's incorrect";
-    answer.className = "incorrect";
-    document.body.append(answer);
+const noButton = document.querySelector(".no");
+noButton.onclick = function () {
+  if (correctAnswer === false) {
+    showAnswerIsCorrect();
+  } else {
+    showAnswerIsIncorrect();
   }
 };
+
+function showAnswerIsCorrect() {
+  const answer = document.createElement("p");
+  answer.textContent = "That's correct";
+  answer.className = "correct";
+  document.body.append(answer);
+  disableButtons();
+}
+function showAnswerIsIncorrect() {
+  const answer = document.createElement("p");
+  answer.textContent = "That's incorrect";
+  answer.className = "incorrect";
+  document.body.append(answer);
+  disableButtons();
+}
+
+function disableButtons() {
+  yesButton.disabled = true;
+  noButton.disabled = true;
+}
